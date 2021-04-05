f483729fb9c5f2e126e63c0f45eafc55496d3a98
Jesus <berriojesus122@gmail.com>
2021-04-04 19:29:47 -0500

    grid de la seccion de card añadido y desordenado

--- src/frontend/pages/home.jsx
+++ src/frontend/pages/home.jsx
@@ -3,17 +3,24 @@ import './styles/home.css';
 
 import Navbar from '../components/navbar';
 import Header from '../components/header';
+import Cards from '../components/cards';
 const Home = () => (
-        
-    <div className="border">
+    <React.Fragment>
+        <div className="border">
         <Navbar></Navbar>
+        <div className="rigthSection">
         <div className="header">
         <Header></Header>
         </div>
+        <div className="cards_container">
+            <Cards></Cards>
+        </div>
+        </div>
+
+        </div>
         
-
-
-    </div>
+    </React.Fragment>
+    
         
     
 )
