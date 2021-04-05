5e16045c3defdb294b7070eb3c44eeedb96f1673
Jesus <berriojesus122@gmail.com>
2021-04-05 13:12:29 -0500

    responsive a cards añadido

--- src/frontend/components/cards.jsx
+++ src/frontend/components/cards.jsx
@@ -2,30 +2,29 @@ import React from 'react';
 import './styles/cards.css';
 
 const Cards = () => (
-    <div className='cards'>
-        <div className="cardsL">
-            <div className="card card_overview"></div>
-            <div className="card card_history"></div>
+    <div className='cards_container'>
+        <div className="cards_leftContainer">
+            <div className="card card_LeftOverview"></div>
+            <div className="card card_LeftHistory"></div>
             </div>
-        <div className="cardsR">
-            <div className="card_goal">
-                <div className="card_goalTitle">
+        <div className="cards_RigthContainer">
+            <div className="card_RightGoal">
+                <div className="card_RightGoalTitle">
                 <p>Goal</p>
                 <svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12.2762 22.7996C17.7991 22.7996 22.2762 18.3225 22.2762 12.7996C22.2762 7.27677 17.7991 2.79962 12.2762 2.79962C6.7534 2.79962 2.27625 7.27677 2.27625 12.7996C2.27625 18.3225 6.7534 22.7996 12.2762 22.7996Z" fill="#FFC145"/>
                     <path d="M12.2762 8.79962V16.7996" stroke="#404040" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M8.27625 12.7996H16.2762" stroke="#404040" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-
             </div>
             </div>
-            <div className="card card_goalTap"></div>
-            <div className="card card_goalTap"></div>
-            <div className="card card_goalTap"></div>
+            <div className="card card_RightGoalTap tap1"></div>
+            <div className="card card_RightGoalTap tap2"></div>
+            <div className="card card_RightGoalTap tap3"></div>
 
-            <div className="card card_statics"></div>
-            <div className="card card_newTransactions"></div>
-            <div className="card card_greate"></div>
+            <div className="card card_RigthStatics"></div>
+            <div className="card card_RigthTransactions"></div>
+            <div className="card card_RigthGreate"></div>
 
             </div>
        
