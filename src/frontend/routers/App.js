fd22efb6775c3486d1404b593f98b4ae24c87c2a
Jesus <berriojesus122@gmail.com>
2021-04-12 00:10:50 -0500

    pagina de login terminada

--- src/frontend/routers/App.js
+++ src/frontend/routers/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/home';
 import Landing from '../pages/landing';
+import LogIn from '../pages/logIn'
 import './styles/main.css';
 const App = () => (
   <BrowserRouter>
       <Switch>
         <Route exact path="/la" component={Home} />
-        <Route exact path="/" component={Landing} />
+        <Route exact path="/s" component={Landing} />
+        <Route exact path="/" component={LogIn} />
       </Switch>
   </BrowserRouter>
 )
